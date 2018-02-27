85ac32ea863e87db91de8f5774ad3a4e0dd97047
Klayton Faria <klayton.souza@gmail.com>
2018-02-27 13:36:52 -0300

    Update start file

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Starting mac setup"
 echo "==========================================="
 
 sudo easy_install pip
-sudo easy_install ansible
+sudo pip install ansible ansible
 ansible-playbook -i ./hosts playbook.yml --verbose
 
 echo "Done!"
