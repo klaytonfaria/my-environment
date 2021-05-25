664c0e7e6576fd6158571d470e0d53dab2e7d23d
Klayton Faria <klayton.souza@gmail.com>
2021-05-25 16:18:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ echo "Starting the mac setup"
 echo "==========================================="
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-python -m pip install --user ansible
+sudo python3 get-pip.py --user
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install --user ansible paramiko
 ansible-playbook -i ./hosts playbook.yml
 
 echo "Done!"
